91991fc00f7f7cc6fbb875fdfba9ba66a9e83b53
WalaaAlrefai <lulusedqi@gmail.com>
2023-05-22 04:58:49 +0000

    Done

--- stack_and_queue/stack_and_queue.py
+++ stack_and_queue/stack_and_queue.py
@@ -28,9 +28,10 @@ class Stack:
         if self.is_empty():
             raise BaseException("The stack is empty")
         else:
-           node = self.top
-           self.top = self.top.next
-           return node.value
+           temp = self.top
+           self.top = temp.next
+           temp.next=None
+           return temp.value
 
     # Space: O(1)
     # Time: O(1)
@@ -49,13 +50,13 @@ class Stack:
         items = ''
 
         while current:
-            items += str(current.value)
+            items +=f"{current.value} -> "
             current = current.next
+         
+        return items +"None"
 
-        return items
 
-
-class Queue():
+class Queue:
 
     # Space: O(1)
     # Time: O(1)
@@ -80,7 +81,8 @@ class Queue():
         if self.is_empty():
             raise BaseException("The stack is empty")
         temp = self.front
-        self.front = self.front.next
+        self.front = temp.next
+        temp.next=None
         return temp.value
 
     # Space: O(1)
@@ -99,7 +101,26 @@ class Queue():
         current = self.front
         items = ''
         while current:
-            items += f"{current.value}"
+            items += f"{current.value} ->"
             current = current.next
 
-        return items
+        return items+"None"
+    
+
+if __name__=="__main__" :
+    stack_01 =Stack()
+    stack_01.push(1)
+    stack_01.push(2)
+    stack_01.push(3)
+    stack_01.push(4)
+    print (stack_01)
+    print (stack_01.top.value)
+    print (stack_01.is_empty())
+    stack_02=Stack()
+    print (stack_02.is_empty())
+    stack_01.push(5)
+    print (stack_01)
+    print(stack_01.pop())
+    print (stack_01)
+
+
